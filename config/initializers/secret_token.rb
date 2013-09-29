3cbcbb0dcf8bf976c3c594caea3f0695e39736f4
Eugenia Kang <enarikang@gmail.com>
2013-09-28 20:44:40 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MindMentor::Application.config.secret_key_base = '7059f4df160bafd4ea962307498589ab1c8fd770d09e66582c86038a1fad46dab3a0b61447bc89dcef2df0f48e525b12f7a0f5c9b9bf766b2a55ea1231e22faf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MindMentor::Application.config.session_store :cookie_store, key: '_mind_mentor_session'
